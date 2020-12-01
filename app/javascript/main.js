335bdb0af34068344afacc2751ad6173406f2064
takumi-murakami <takumi28280503@gmail.com>
2020-12-01 11:25:28 +0900

    initial commit!

---
+++ app/javascript/main.js
@@ -0,0 +1,36 @@
+function main() {
+  
+    var vm = new Vue({
+      el: '#app',
+      data: {
+        newItem: '',
+        todos: [{
+          title: 'task 1',
+          isDone: false
+        }, {
+          title: 'task 2',
+          isDone: false
+        }, {
+          title: 'task 3',
+          isDone: true
+        }]
+      },
+      methods: {
+        addItem: function() {
+          var item = {
+            title: this.newItem,
+            isDone: false
+          };
+          this.todos.push(item);
+          this.newItem = '';
+        },
+        deleteItem: function(index) {
+          if (confirm('are you sure?')) {
+            this.todos.splice(index, 1);
+          }
+        }
+      }
+    });
+    
+}
+window.addEventListener("load", main);

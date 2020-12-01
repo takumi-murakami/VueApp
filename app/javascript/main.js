4a6127ad8dbc1979de883867a0cd2c019bb76251
takumi-murakami <takumi28280503@gmail.com>
2020-12-01 12:02:56 +0900

    local strage保存処理完了

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,36 +1,73 @@
 function main() {
   
-    var vm = new Vue({
-      el: '#app',
-      data: {
-        newItem: '',
-        todos: [{
-          title: 'task 1',
-          isDone: false
-        }, {
-          title: 'task 2',
-          isDone: false
-        }, {
-          title: 'task 3',
-          isDone: true
-        }]
-      },
-      methods: {
-        addItem: function() {
-          var item = {
-            title: this.newItem,
-            isDone: false
-          };
-          this.todos.push(item);
-          this.newItem = '';
+  var vm = new Vue({
+    el: '#app',
+    data: {
+      newItem: '',
+      todos: [{
+        title: 'task 1',
+        isDone: false
+      }, {
+        title: 'task 2',
+        isDone: false
+      }, {
+        title: 'task 3',
+        isDone: true
+      }]
+    },
+    watch: {
+      // todos: function() {
+      //   localStorage.setItem('todos', JSON.stringify(this.todos));
+      //   alert('Data saved!');
+      // }
+      todos: {
+        handler: function() {
+          localStorage.setItem('todos', JSON.stringify(this.todos));
+          // alert('Data saved!');
         },
-        deleteItem: function(index) {
-          if (confirm('are you sure?')) {
-            this.todos.splice(index, 1);
-          }
-        }
+        deep: true
       }
-    });
-    
+    },
+    mounted: function() {
+      this.todos = JSON.parse(localStorage.getItem('todos')) || [];
+    },
+    methods: {
+      addItem: function() {
+        var item = {
+          title: this.newItem,
+          isDone: false
+        };
+        this.todos.push(item);
+        this.newItem = '';
+      },
+      deleteItem: function(index) {
+        if (confirm('are you sure?')) {
+          this.todos.splice(index, 1);
+        }
+      },
+      purge: function() {
+        if (!confirm('delete finished?')) {
+          return;
+        }
+        // this.todos = this.todos.filter(function(todo) {
+        //   return !todo.isDone;
+        // });
+        this.todos = this.remaining;
+      }
+    },
+    computed: {
+      remaining: function() {
+        // var items = this.todos.filter(function(todo) {
+        //   return !todo.isDone;
+        // });
+        // return items.length;
+        return this.todos.filter(function(todo) {
+          return !todo.isDone;
+        });
+      }
+    }
+  });
+
 }
 window.addEventListener("load", main);
+
